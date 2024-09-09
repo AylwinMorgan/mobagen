5ee7fdc1335ed774f0f54b4551f4b5c7d6c36847
Aylwin <aylwinmorgan@gmail.com>
2024-09-08 22:27:51 -0400

    Small tweak to alignment rule.

--- examples/flocking/behaviours/AlignmentRule.cpp
+++ examples/flocking/behaviours/AlignmentRule.cpp
@@ -4,6 +4,7 @@
 Vector2f AlignmentRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
   // Try to match the heading of neighbors = Average velocity
   Vector2f averageVelocity = Vector2f::zero();
+  float forceScalar = 3.0f;
 
   // todo: add your code here to align each boid in a neighborhood
   // hint: iterate over the neighborhood
@@ -13,5 +14,5 @@ Vector2f AlignmentRule::computeForce(const std::vector<Boid*>& neighborhood, Boi
   if (neighborhood.size() != 0) {
     averageVelocity /= neighborhood.size();
   }
-  return Vector2f::normalized(averageVelocity);
+  return Vector2f::normalized(averageVelocity) * forceScalar;
 }
