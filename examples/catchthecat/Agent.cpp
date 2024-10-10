636829891bc68845276ad38af0e60280a89732a1
Aylwin <aylwinmorgan@gmail.com>
2024-10-09 21:28:52 -0400

    Reworked agent.cpp to use A* pathfinding

--- examples/catchthecat/Agent.cpp
+++ examples/catchthecat/Agent.cpp
@@ -4,42 +4,65 @@
 #include <queue>
 #include "World.h"
 using namespace std;
+
+struct Point2DWithDist {
+  Point2D point = Point2D(0,0);
+  int accDistance = 0;
+  int heuristicDistance = 0;
+
+  bool operator<(const Point2DWithDist& rhs) const { 
+    return (accDistance + heuristicDistance > rhs.accDistance + rhs.heuristicDistance);
+  }
+};
+
 std::vector<Point2D> Agent::generatePath(World* w) {
   unordered_map<Point2D, Point2D> cameFrom;  // to build the flowfield and build the path
-  queue<Point2D> frontier;                   // to store next ones to visit
+  priority_queue<Point2DWithDist> frontier;                   // to store next ones to visit
   unordered_set<Point2D> frontierSet;        // OPTIMIZATION to check faster if a point is in the queue
   unordered_map<Point2D, bool> visited;      // use .at() to get data, if the element dont exist [] will give you wrong results
-
+  
+  //unordered_map<Point2DWithDist, int> distanceCost;
 
   // bootstrap state
-  auto catPos = w->getCat();
+  Point2DWithDist catPos;
+  catPos.point = w->getCat();
+  catPos.accDistance = 0;
+  catPos.heuristicDistance = getManhattanDistanceToBorder(catPos.point,w);
+
   frontier.push(catPos);
-  frontierSet.insert(catPos);
+  frontierSet.insert(catPos.point);
   Point2D borderExit = Point2D::INFINITE;  // if at the end of the loop we dont find a border, we have to return random points
+  
+  
 
   while (!frontier.empty()) {
-    Point2D current = frontier.front();
+    Point2DWithDist current = frontier.top();
     frontier.pop();
     // get the current from frontier
     // remove the current from frontierset
-    frontierSet.erase(current);
+    frontierSet.erase(current.point);
     // mark current as visited
-    visited[current] = true;
+    visited[current.point] = true;
     // getVisitableNeightbors(world, current) returns a vector of neighbors that are not visited, not cat, not block, not in the queue
     std::vector<Point2D> neighbors;
     std::vector<Point2D> visitableNeighbors;
 
-    neighbors = w->neighbors(current);
+    neighbors = w->neighbors(current.point);
 
     for (int i = 0; i < neighbors.size(); i++) {
       if (!w->isValidPosition(neighbors[i])) {
-        borderExit = current;
+        borderExit = current.point;
       }
         // point is not cat                 // point has no wall                     // point is not in queue                   // point has not been visited
       else if (w->getCat() != neighbors[i] && !w->getContent(neighbors[i]) && !frontierSet.contains(neighbors[i]) && !visited[neighbors[i]]) {
         visitableNeighbors.push_back(neighbors[i]);
-        cameFrom[neighbors[i]] = current;
-        frontier.push(neighbors[i]);
+        cameFrom[neighbors[i]] = current.point;
+        Point2DWithDist newPoint;
+        newPoint.point = neighbors[i];
+        newPoint.accDistance = current.accDistance + 1;
+        newPoint.heuristicDistance = getManhattanDistanceToBorder(newPoint.point, w);
+
+        frontier.push(newPoint);
         frontierSet.insert(neighbors[i]);
       }
     }
@@ -81,9 +104,9 @@ std::vector<Point2D> Agent::generatePath(World* w) {
 // heuristic function for A* pathfinding
 int Agent::getManhattanDistanceToBorder(Point2D point,World* world) { 
     if (abs(point.x) > abs(point.y)) {
-    return world->getWorldSideSize()/2;
+        return world->getWorldSideSize()/2 - abs(point.x);
     }
     else {
-    
+      return world->getWorldSideSize() / 2 - abs(point.y);
     }
 }
--- examples/catchthecat/Catcher.cpp
+++ examples/catchthecat/Catcher.cpp
@@ -5,17 +5,14 @@
 #include <queue>
 
 Point2D Catcher::Move(World* world) {
-  
-  //TODO: replace algorithm with A* with heuristic of manhattan distance to border
-
   // first check if cat is one step from winning and fill in the winning space if needed
   // otherwise check if the cat is two steps from winning on a tile that has less than 2 filled border neighbors, fill if needed
   // otherwise check if there is a border space with no border neighbors, if there is, fill the closest one
   // otherwise fill the cats target (closest border tile) if one is reachable
   // otherwise fill randomly
 
-  // watch out for top and bottom right corners, they require special logic
-
+  // watch out for corners, they require special logic
+  
   vector<Point2D> catPath = generatePath(world);
   Point2D catTarget = catPath.front();
   vector<Point2D> borders = world->getBorderSpaces();
@@ -26,19 +23,21 @@ Point2D Catcher::Move(World* world) {
   if (catPath.size() == 1) {
     return catTarget;
   }
+
   for (int i = 0; i < numberOfBorders; i++) {
-    if (world->getContent(borders[(i + targetBorderIndex) % numberOfBorders])) {
+    int baseIndex = i / 2 * pow(-1, i) + numberOfBorders;
+    if (world->getContent(borders[(baseIndex + targetBorderIndex) % numberOfBorders])) {
       continue;
     }
     int numberOfAdjacentBorderWalls = 0;
-    if (world->getContent(borders[(i+targetBorderIndex - 1) % numberOfBorders])) {
+    if (world->getContent(borders[(baseIndex + targetBorderIndex - 1) % numberOfBorders])) {
       numberOfAdjacentBorderWalls++;
     }
-    if (world->getContent(borders[(i+targetBorderIndex + 1) % numberOfBorders])) {
+    if (world->getContent(borders[(baseIndex + targetBorderIndex + 1) % numberOfBorders])) {
       numberOfAdjacentBorderWalls++;
     }
     if (numberOfAdjacentBorderWalls == 0 || (numberOfAdjacentBorderWalls < 2 && i == 0 && catPath.size() == 2)) {
-      return borders[(targetBorderIndex + i)%numberOfBorders];
+      return borders[(targetBorderIndex + baseIndex) % numberOfBorders];
     }
   }
 
